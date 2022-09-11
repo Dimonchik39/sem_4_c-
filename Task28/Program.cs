d83f37514941e1392ea3b829fd0c163ec901ed31
Dimonchik <sudjadred@live.com>
2022-09-11 19:55:00 +0200

    add_Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int MultNumbers(int num)
+{
+    if (num != 0)
+    {
+        if (num < 0) num = -num;
+        int sum = 1;
+        for (int i = 1; i <= num; i++)
+    {
+        sum *= i;
+    }
+        return sum;
+    }
+    return 1;
+}
+
+int multNumbers = MultNumbers(number);
+Console.WriteLine($"Факториал числа {number} = {multNumbers}");
